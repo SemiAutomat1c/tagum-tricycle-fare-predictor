3dbee088091795c45f9b4d523a9b92c8cc846591
Ryan Christian D. Deniega <r.deniega.140107.tc@umindanao.edu.ph>
2026-06-01 02:30:21 +0800

    Fix Leaflet tile layer URL format by removing retina placeholder

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function initializeMap() {
     map = L.map('map').setView([CONFIG.tagumCity.lat, CONFIG.tagumCity.lng], CONFIG.defaultZoom);
 
     // Add CartoDB Dark Matter tile layer
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 20
