fef9e99fb9ecf4bd772a7ecdade528614285d10b
Ryan Christian D. Deniega <r.deniega.140107.tc@umindanao.edu.ph>
2026-06-01 02:24:36 +0800

    Remove all Firebase configuration files and dependencies

--- api/main.py
+++
@@ -1,8 +0,0 @@
-from firebase_functions import https_fn
-from index import app
-
-# Expose the Flask app as a Cloud Function
-@https_fn.on_request()
-def api(req: https_fn.Request) -> https_fn.Response:
-    with app.request_context(req.environ):
-        return app.full_dispatch_request()
